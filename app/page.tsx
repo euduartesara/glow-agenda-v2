b27e17b246b79f11a276627bee77f460e807b3ce
David <david.contato62@gmail.com>
2025-12-12 01:25:05 -0300

    pagina About

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
 import Services from "./components/Services";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
+      <About />
       <Contact />
     </>
   );
